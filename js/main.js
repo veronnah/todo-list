5f66b68b6be852b76042168d45c15b055a9a7d9b
veronnah <yoo.veroon@gmail.com>
2020-06-08 17:36:50 +0200

    4.0

--- js/main.js
+++ js/main.js
@@ -5,22 +5,22 @@ const form = document.querySelector(".form__new-task");
 
 const tasksJSON = localStorage.getItem('tasks');
 
-if(tasksJSON){
+if (tasksJSON) {
 
     let tasksArray = JSON.parse(tasksJSON);
     tasks = tasksArray;
-    tasks.forEach(function(item){
+    tasks.forEach(function (item) {
 
-    const taskHTML = `
-    <li class="list-item">
+        const taskHTML = `
+    <li class="list-item" data-action = "add-class">
     <span class="task-title">${item}</span>
-    <button type="button" data-action="delete-task" class="btn__delete-task">delete</button>
+    <button type="button" data-action="delete-task" class="btn__delete-task">✘</button>
     </li>
     `;
 
-    tasksList.insertAdjacentHTML('beforeend', taskHTML);
+        tasksList.insertAdjacentHTML('beforeend', taskHTML);
     });
-  
+
 }
 
 form.addEventListener('submit', function (event) {
@@ -32,35 +32,61 @@ form.addEventListener('submit', function (event) {
 
     tasks.push(taskText);
 
-    localStorage.setItem('tasks', JSON.stringify(tasks));   
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 
     const taskHTML = `
-    <li class="list-item">
+    <li class="list-item" data-action = "add-class">
     <span class="task-title">${taskText}</span>
-    <button type="button" data-action="delete-task" class="btn__delete-task">delete</button>
+    <button type="button" data-action="delete-task" class="btn__delete-task">✘</button>
     </li>
     `;
 
     tasksList.insertAdjacentHTML('beforeend', taskHTML);
 
     taskInput.value = "";
+    let listItem = document.querySelectorAll(".task-title");
+    for (let i = 0; i < listItem.length; i++) {
+        const liItem = listItem[i];
+        liItem.onclick = function () {
+            if (this.classList.contains("task-title")) {
+                this.classList.add("text-decor");
+            } else {
+                this.classList.add("task-title");
+                this.classList.remove("text-decor");
+            }
+        }
 
-});
-
-tasksList.addEventListener('click', function(event){
-
-    if(event.target.getAttribute("data-action") === "delete-task"){
-
-    const taskText = event.target.closest('li').querySelector('.task-title').textContent;
-
-    const taskIndex = tasks.indexOf(taskText);
-
-    tasks.splice(taskIndex, 1);
-
-    event.target.parentElement.remove();
-
-    localStorage.setItem('tasks', JSON.stringify(tasks));
     }
 
 });
 
+tasksList.addEventListener('click', function (event) {
+
+    if (event.target.getAttribute("data-action") === "delete-task") {
+
+        const taskText = event.target.closest('li').querySelector('.task-title').textContent;
+
+        const taskIndex = tasks.indexOf(taskText);
+
+        tasks.splice(taskIndex, 1);
+
+        event.target.parentElement.remove();
+
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    }
+});
+
+let listItem = document.querySelectorAll(".task-title");
+for (let i = 0; i < listItem.length; i++) {
+    const liItem = listItem[i];
+    liItem.onclick = function () {
+        if (this.classList.contains("task-title")) {
+            this.classList.add("text-decor");
+        } else {
+            this.classList.add("task-title");
+            this.classList.remove("text-decor");
+        }
+    }
+
+}
